ff3806b880de479c709c22cf88471c3a005739a9
Halit Bildirici <halit.bildirici@duke.edu>
2023-07-27 13:12:56 +0300

    started the project

---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      <p>This is a React component integrated into the existing project.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
